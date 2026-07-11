d6f5947e60a6d1764bc7e6dd6d1baa7954a522de
piotrzdziarski <pzdziarski@protonmail.com>
2026-07-11 07:53:55 +0200

    1.18 and written an info about Polish language to the README.md

---
+++ 1.18.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+#define MAX_LINE_LENGTH 100
+#define MAX_LINES 100
+int getLine(char line[], int maxline);
+void copy(char to[], char from[]);
+main() {
+	int len, lineIndex = 0, c;
+	char line[MAX_LINE_LENGTH];
+	char lines[MAX_LINES][MAX_LINE_LENGTH];
+	int linesLength[MAX_LINES];
+
+	while ((len = getLine(line, MAX_LINE_LENGTH)) > 0)
+		if (len == MAX_LINE_LENGTH - 1) {
+			while ((c = getchar()) != EOF && c != '\n') len++;
+			linesLength[lineIndex] = len;
+			line[MAX_LINE_LENGTH - 2] = '\n'; 
+			copy(lines[lineIndex], line);
+			lineIndex++;
+		}
+		else if (len == 1 && line[0] == '\n');
+		else {
+			int i;
+			for (i = len - 2; line[i] == ' ' || line[i] == '\t'; i--)
+				line[i] = '\0';
+			line[i + 2] = '\0';
+			line[i + 1] = '\n';
+			copy(lines[lineIndex], line);
+			lineIndex++;
+		}
+
+	printf("Maksymalna długość wiersza: %d\n", MAX_LINE_LENGTH);
+	printf("Przy za długim wierszu wypisuje się długość wiersza i tylko tyle tekstu ile możliwe; bez usuwania białych znaków na końcu wiersza.\n");
+	for (int i = 0; i < lineIndex; i++) {
+		if (linesLength[i] > MAX_LINE_LENGTH)
+			printf("%d: ", linesLength[i]);
+		printf("%s", lines[i]);		
+	}
+}
+getLine(char line[], int maxline) {
+	int c, i;
+
+	for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
+void copy(char to[], char from[]) {
+	int i = 0;
+	while ((to[i] = from[i]) != '\0') ++i;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-The repository contains tasks from the book "Language ANSI C" of Brian W. Kernighan and Dennis M. Ritchie (Wydawnictwo Naukowo-Techniczne: Warszawa 2007, Wyd. X).
+The repository contains tasks from the book "The C Programming Language. Second Edition" ("Język ANSI C") of Brian W. Kernighan and Dennis M. Ritchie (Wydawnictwo Naukowo-Techniczne: Warszawa 2007, Wyd. X).
+Printed texts are written in Polish.
