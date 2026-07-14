7041625576b9951ef52793b0cac5afb9a726622b
piotrzdziarski <pzdziarski@protonmail.com>
2026-07-14 14:01:56 +0200

    1.20 with tabulation point's control

--- 1.20.c
+++ 1.20.c
@@ -1,3 +1,3 @@
 #include"stdio.h"
-#define TAB 4
-main(){char c;while((c=getchar())!=EOF)if(c=='\t')for(int i=0;i<TAB;i++)putchar(' ');else putchar(c);}
+#define TAB 8
+main(){char c;int tabPoint=0;while((c=getchar())!=EOF){if(c=='\t'){for(int i=0;i<TAB-tabPoint;i++)putchar(' ');tabPoint=0;}else{putchar(c);tabPoint++;if(tabPoint==8)tabPoint=0;if(c=='\n')tabPoint=0;}}}
