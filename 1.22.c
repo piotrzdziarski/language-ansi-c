746bd5e4f0f6a6578d7f254fc678e78f014a5376
piotrzdziarski <pzdziarski@protonmail.com>
2026-07-21 20:07:18 +0200

    Improved the 1.22 task

--- 1.22.c
+++ 1.22.c
@@ -1,27 +1,62 @@
 #include "stdio.h"
-#include "limits.h"
-#define BREAK 50
+#define BREAK 10
 #define TAB 8
 main() {
 	char line[BREAK], c, /*Previous character*/pc = 0;
+	for (int j = 0; j < BREAK; j++) line[j] = 0;
 	short 
-		i = 0;
+		i = 0,
 		/*Tab point*/tp = 0,
 		/*Non-Tab character*/ntc = 0,
-		/*Index of last character different than Tab or Space*/pi;
+		/*Index of last character different than Tab or Space*/li;
 	while ((c = getchar()) != EOF) {
 		line[i] = c;
 		if (c == '\t' || c == ' ') {
 			if (pc != '\t' && pc != ' ')
-				pi = i - 1;
-			if (c != '\t') {
-				tp++;
+				li = i - 1;
+			if (c == '\t') {
+				/*for (int j = i; j < i + TAB - tp; j++)
+					line[j] = 0;*/
+				i += TAB - tp;
+				tp = 0;
+			} else {
+				tp++; if (tp == TAB) tp = 0;
 			}
 		}
+		else if (c == '\n') {
+			for (int j = 0; j <= i; j++)
+				if (line[j])
+					putchar(line[j]);
+			for (int j = 0; j < BREAK; j++) line[j] = 0;
+			i = 0;
+			continue;
+		}
 		else {
-			ntc++;
-			tp++;	
+			tp++; if (tp == TAB) tp = 0;
 		}
 		pc = c;
+		i++;
+		if (i == BREAK) {
+			if (li) {
+				for (int j = 0; j <= li; j++)
+					if (line[j])
+						putchar(line[j]);
+				putchar('\n');
+				int k = 0;
+				for (int j = li + 1; j < BREAK; ++j, ++k)
+					line[k] = line[j];
+				for (; k < BREAK; k++)
+					line[k] = 0;
+				i = BREAK - li;
+				li = 0;
+			} else {
+				for (int j = 0; j < BREAK; j++) {
+					putchar(line[j]);
+					line[j] = 0;
+				}
+				putchar('\n');
+				i = 0;
+			}
+		}
 	}
 }
